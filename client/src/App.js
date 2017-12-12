9e8abc660a1736339ec7074bcfc785ff8c59fff2
Antti <anttis.pitkanen@gmail.com>
2017-12-12 20:01:07 +0200

    add proxy and test it

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      course: null
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch('/course?id=36903');
+    const resJSON = await response.json();
+    this.setState({ course: resJSON });
+    console.log(resJSON);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="App-intro">
+          <li>Id: {this.state.course ? this.state.course.id : 'loading...'}</li>
+          <li>Course name: {this.state.course ? this.state.course.name : 'loading...'}</li>
+        </ul>
       </div>
     );
   }
