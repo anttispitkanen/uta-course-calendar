6ed5268eb5e1cdf9a217b2666234dc5f2fd05300
Antti <anttis.pitkanen@gmail.com>
2018-01-01 23:37:50 +0200

    header styles with uta logo

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import utaLogo from '../../images/utaen-small.png';
 import './Header.scss';
 
 const Header = () => (
     <header className="main-header">
         <h1 className="main-header-heading">Uta course calendar</h1>
+        <img src={utaLogo} alt="University of Tampere logo" className="main-header-logo" />
     </header>
 );
 
