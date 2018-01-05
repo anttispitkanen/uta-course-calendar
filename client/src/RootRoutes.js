69f3e3b80fea7d71cdc0648fe5c7e523bed084a3
Antti <anttis.pitkanen@gmail.com>
2018-01-05 15:16:17 +0200

    add note

--- client/src/RootRoutes.js
+++ client/src/RootRoutes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import MockPreview from './mockPreview/MockPreview';
+// import MockPreview from './mockPreview/MockPreview';
 import Preview from './components/preview';
 
 // TODO: add Help component (a modal with instructions on how to use)
+// also it should not be a switch but an overlaying component
 const RootRoutes = () => (
     <Switch>
         <Route path="/help" component={() => <div>Help here</div>} />
