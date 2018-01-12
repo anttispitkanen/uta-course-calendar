5423fe60b6b5933248ddb10f4689607e65b60bed
Antti <anttis.pitkanen@gmail.com>
2018-01-12 18:46:18 +0200

    updateInfo works as UTA server certs are fixed

--- updateInfo.js
+++ updateInfo.js
@@ -7,21 +7,20 @@ const API_URL = 'https://opendata.uta.fi:8443/apiman-gateway/UTA/opintojaksot/1.
 
 axios.defaults.headers.get['X-API-key'] = API_KEY;
 
-// FIXME: try this
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-// TODO: ok so this works
-
 const getCourses = async () => {
     try {
         const response = await axios.get(API_URL);
         const data = await response.data;
         fs.writeFile('./courses.json', JSON.stringify(data), err => {
-            if (err) console.error(err);
+            if (err) throw Error(err);
         });
 
     } catch (e) {
-        console.error(e);
+        throw Error(e);
     }
 }
 
-getCourses();
+console.info('\n\nUpdating courses...\n\n');
+getCourses()
+.then(() => console.info('\n\nCourses updated!\n\n'))
+.catch(err => console.error(err));
