81d99d9b1ac4818443a64d41b413c6003c5111b0
Antti <anttis.pitkanen@gmail.com>
2018-01-14 00:09:06 +0200

    header styling

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../images/uta-course-calendar-280.png';
 import './Header.scss';
 
@@ -8,6 +9,11 @@ const Header = () => (
             <img src={logo} className="main-header-logo" alt="logo" />
             <h1 className="main-header-heading">Uta course calendar</h1>
         </div>
+
+        <Link to="/help" className="header-help-link">
+            <span className="link-text">What is this?</span>
+            <i className="link-icon fa fa-question-circle" />
+        </Link>
     </header>
 );
 
