f613918a36c403ca890a9e7473bcc6e5088404d7
Antti <anttis.pitkanen@gmail.com>
2018-01-14 15:51:52 +0200

    HowToUse and example pics

--- client/src/components/howToUse/HowToUse.js
+++ client/src/components/howToUse/HowToUse.js
@@ -1,5 +1,9 @@
 import React, { Component} from 'react';
 import onClickOutside from 'react-onclickoutside';
+import example1 from '../../images/example-1.png';
+import example2 from '../../images/example-2.png';
+import example3 from '../../images/example-3.png';
+import example4 from '../../images/example-4.png';
 import './HowToUse.scss';
 
 class HowToUse extends Component {
@@ -19,10 +23,69 @@ class HowToUse extends Component {
                     <i className="howto-close fa fa-times"
                         onClick={this.closeModal} />
                 </div>
-                How to use
+
+                <h2>How to use?</h2>
+
+                <div className="howto-section">
+                    <i className="howto-section-number">1.</i>
+                    <p className="howto-section-text">
+                        Find the course you want to have in your calendar in
+                        the <TeachingLink lang="en">UTA teaching schedule
+                        </TeachingLink> (<TeachingLink lang="fi">Finnish one here</TeachingLink>).
+                    </p>
+                </div>
+
+                <div className="howto-section">
+                    <i className="howto-section-number">2.</i>
+                    <p className="howto-section-text">
+                        Copy the course URL from the address bar.
+                    </p>
+                    <img className="howto-image" src={example1} alt="example 1"/>
+                </div>
+
+                <div className="howto-section">
+                    <i className="howto-section-number">3.</i>
+                    <p className="howto-section-text">
+                        Paste the URL into the search field and hit "Search".
+                    </p>
+                    <img className="howto-image" src={example2} alt="example 2"/>
+                </div>
+
+                <div className="howto-section">
+                    <i className="howto-section-number">4.</i>
+                    <p className="howto-section-text">
+                        Select the groups you wish to include in your calendar
+                        (lectures, study groups, seminars...) and click "Download"
+                        to download the calendar as an .ics-file.
+                    </p>
+                    <img className="howto-image" src={example3} alt="example 3"/>
+                </div>
+
+                <div className="howto-section">
+                    <i className="howto-section-number">5.</i>
+                    <p className="howto-section-text">
+                        Import the .ics-file to your calendar of choice, and voilà!
+                        All the classes are automatically added to your calendar as events!
+                    </p>
+                    <img className="howto-image" src={example4} alt="example 4"/>
+                    <p>
+                        Google will help you here with instructions if needed.
+                    </p>
+                </div>
             </div>
         );
     }
 }
 
 export default onClickOutside(HowToUse);
+
+const TeachingLink = ({ lang, children }) => (
+    <a target="_blank" rel="noopener noreferer"
+        href={
+            lang === 'fi'
+                ? 'https://www10.uta.fi/opas/opetusohjelma/index.htm'
+                : 'https://www10.uta.fi/opas/teaching/index.htm?uiLang=en&kieli=en'
+        }>
+            {children}
+    </a>
+);
