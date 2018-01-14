9fe9a092416d76df5b060f7001529ca0099cfc0e
Antti <anttis.pitkanen@gmail.com>
2018-01-14 18:49:02 +0200

    force HTTPS in production

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`listening on ${PORT} in ${process.env.NODE_ENV} mode`));
 
+// force HTTPS redirect
+if (process.env.NODE_ENV === 'production') {
+    app.all('*', (req, res, next) => {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            res.redirect(`https://${req.get('host')}${req.url}`);
+        } else {
+            next();
+        }
+    });
+}
+
 app.get('/test', async (req, res) => {
     res.json({ message: 'API works whoopee :DD' })
 });
