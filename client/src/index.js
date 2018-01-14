9b2b918e0335f5d3d8636f24a3cc27d4fb70567a
Antti <anttis.pitkanen@gmail.com>
2018-01-14 22:24:09 +0200

    add client side redirect to HTTPS in production

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// always redirect to HTTPS in production
+if (process.env.NODE_ENV === 'production') {
+    const { protocol, hostname, port, pathname } = window.location;
+    if (protocol !== 'https:') {
+        window.location.replace('https://' + hostname + (port ? ':' + port : '') + pathname);
+    }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
